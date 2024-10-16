3474bff7b91377b773f80570b20a4598607d1f9c
Joel <sygullajoel@gmail.com>
2024-10-16 09:31:37 +0200

    fix: Adjust date input to German format (dd.mm.yyyy) in user form

--- src/app/dialog-add-user/dialog-add-user.component.ts
+++ src/app/dialog-add-user/dialog-add-user.component.ts
@@ -1,30 +1,60 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { User } from '../models/user.class';
-import { SharedMaterialModule } from '../shared-material.module';
-import { MatDatepickerModule } from '@angular/material/datepicker';
 import { MatDialogRef } from '@angular/material/dialog';
 import { UserService } from '../services/user.service';
+import { SharedMaterialModule } from '../shared-material.module';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import {
+  MAT_DATE_FORMATS,
+  MAT_DATE_LOCALE,
+  DateAdapter,
+} from '@angular/material/core';
+import { MatNativeDateModule } from '@angular/material/core';
+import { User } from '../models/user.class';
+
+export const MY_DATE_FORMATS = {
+  parse: {
+    dateInput: 'DD.MM.YYYY',
+  },
+  display: {
+    dateInput: 'DD.MM.YYYY',
+    monthYearLabel: 'MMMM YYYY',
+    dateA11yLabel: 'LL',
+    monthYearA11yLabel: 'MMMM YYYY',
+  },
+};
 
 @Component({
   selector: 'app-dialog-add-user',
   templateUrl: './dialog-add-user.component.html',
   styleUrls: ['./dialog-add-user.component.scss'],
-  imports: [SharedMaterialModule, MatDatepickerModule],
-  providers: [MatDatepickerModule],
+  imports: [SharedMaterialModule, MatDatepickerModule, MatNativeDateModule],
+  providers: [
+    { provide: MAT_DATE_LOCALE, useValue: 'de-DE' },
+    { provide: MAT_DATE_FORMATS, useValue: MY_DATE_FORMATS },
+  ],
   standalone: true,
 })
 export class DialogAddUserComponent implements OnInit {
   userForm!: FormGroup;
   isSubmitting = false;
+  minDate: Date;
+  maxDate: Date;
 
   constructor(
     private fb: FormBuilder,
     private dialogRef: MatDialogRef<DialogAddUserComponent>,
     private userService: UserService,
-  ) {}
+    private dateAdapter: DateAdapter<Date>,
+  ) {
+    const currentYear = new Date().getFullYear();
+    this.minDate = new Date(1900, 0, 1);
+    this.maxDate = new Date(currentYear, 11, 31);
+  }
 
   ngOnInit(): void {
+    this.dateAdapter.setLocale('de-DE');
+
     this.userForm = this.fb.group({
       firstName: ['', Validators.required],
       lastName: ['', Validators.required],
--- src/app/dialog-edit-user/dialog-edit-user.component.ts
+++ src/app/dialog-edit-user/dialog-edit-user.component.ts
@@ -6,26 +6,58 @@ import { User } from '../models/user.class';
 import { SharedMaterialModule } from '../shared-material.module';
 import { MatDatepickerModule } from '@angular/material/datepicker';
 import { Timestamp } from 'firebase/firestore';
+import {
+  MAT_DATE_FORMATS,
+  MAT_DATE_LOCALE,
+  DateAdapter,
+  MatNativeDateModule,
+} from '@angular/material/core';
+
+// German Date format configuration
+export const MY_DATE_FORMATS = {
+  parse: {
+    dateInput: 'DD.MM.YYYY',
+  },
+  display: {
+    dateInput: 'DD.MM.YYYY',
+    monthYearLabel: 'MMMM YYYY',
+    dateA11yLabel: 'LL',
+    monthYearA11yLabel: 'MMMM YYYY',
+  },
+};
 
 @Component({
   selector: 'app-dialog-edit-user',
   templateUrl: './dialog-edit-user.component.html',
   styleUrls: ['./dialog-edit-user.component.scss'],
+  imports: [SharedMaterialModule, MatDatepickerModule, MatNativeDateModule],
+  providers: [
+    { provide: MAT_DATE_LOCALE, useValue: 'de-DE' },
+    { provide: MAT_DATE_FORMATS, useValue: MY_DATE_FORMATS },
+  ],
   standalone: true,
-  imports: [SharedMaterialModule, MatDatepickerModule],
 })
 export class DialogEditUserComponent implements OnInit {
   userForm!: FormGroup;
   isSubmitting = false;
+  minDate: Date;
+  maxDate: Date;
 
   constructor(
     private fb: FormBuilder,
     private dialogRef: MatDialogRef<DialogEditUserComponent>,
     private userService: UserService,
     @Inject(MAT_DIALOG_DATA) public data: { user: User },
-  ) {}
+    private dateAdapter: DateAdapter<Date>,
+  ) {
+    const currentYear = new Date().getFullYear();
+    this.minDate = new Date(1900, 0, 1);
+    this.maxDate = new Date(currentYear, 11, 31);
+  }
 
   ngOnInit(): void {
+    this.dateAdapter.setLocale('de-DE');
+
     this.userForm = this.fb.group({
       firstName: [this.data.user.firstName, Validators.required],
       lastName: [this.data.user.lastName, Validators.required],
